395086159e61e04b34060e12855c63277a9cfd87
Tim GF63 <tim@shatzir.com>
2022-03-02 19:59:57 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import wrap,time
+wrap.world.create_world(400,400,200,200)
+ppackmen=wrap.sprite.add("pacman",100,200,"player2")
+
+@wrap.on_key_down(wrap.K_RIGHT)
+def right():
+    anglea=wrap.sprite.get_angle(ppackmen)
+    wrap.sprite.set_angle(ppackmen,anglea+45)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
